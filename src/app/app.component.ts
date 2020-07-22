c7490646954d2826e358ce4b99268bd31d0697b8
mkarasinski <maciej.karasinski.95@gmail.com>
2020-07-22 13:33:55 +0200

    pipes app finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,38 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string;
+  date: string;
+  amount: number;
+  height: number;
+  miles: number;
+
+  car = {
+    mark: 'Toyota',
+    model: 'Yaris',
+    year: 2010,
+  };
+
+  onMilesChange(value: string) {
+    this.miles = parseFloat(value);
+  }
+
+  onHeightChange(value: string) {
+    this.height = parseFloat(value);
+  }
+
+  onNameChange(value: string) {
+    this.name = value;
+  }
+
+  onDateChange(value: string) {
+    this.date = value;
+  }
+
+  onAmountChange(value: string) {
+    this.amount = parseFloat(value);
+  }
 }
