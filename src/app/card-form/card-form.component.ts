bbf955120bf31aa066da48518c0c967fa14b19eb
Maciej <maciej.karasinski.95@gmail.com>
2020-10-11 21:35:26 +0200

    CreditCard App finished

---
+++ src/app/card-form/card-form.component.ts
@@ -0,0 +1,46 @@
+import { DateFormControl } from './../date-form-control';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-card-form',
+  templateUrl: './card-form.component.html',
+  styleUrls: ['./card-form.component.css']
+})
+export class CardFormComponent implements OnInit {
+  cardForm = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+    ]),
+    cardNumber: new FormControl('', [
+      Validators.required,
+      Validators.minLength(16),
+      Validators.maxLength(16)
+    ]),
+    expiration: new DateFormControl('', [
+      Validators.required,
+      Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/)
+    ]),
+    securityCode: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(3),
+      Validators.pattern(/^[0-9]+$/)
+    ])
+  });
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    console.log('Form was submitted');
+  }
+
+  onResetClick(): void {
+    this.cardForm.reset();
+  }
+
+}
---
+++ src/app/input/input.component.ts
@@ -0,0 +1,23 @@
+import { FormControl } from '@angular/forms';
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css']
+})
+export class InputComponent implements OnInit {
+  @Input() control: FormControl;
+  @Input() label: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  showErrors () {
+    const { touched, errors} = this.control;
+    return touched && errors;
+  }
+
+}
